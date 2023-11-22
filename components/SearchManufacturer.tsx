25e6d3c41e06d6fde29e3d38c0da70a0983bcacb
zephyrus1009 <zephyrus1009@gmail.com>
2023-11-22 15:32:23 +0700

    giai thich xong searchbar va searchmanufacturer

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,3 +1,4 @@
+//component này là con của component SearchBar
 import Image from "next/image";
 import { Fragment, useState } from "react";
 import { Combobox, Transition } from "@headlessui/react";
@@ -5,7 +6,9 @@ import { Combobox, Transition } from "@headlessui/react";
 import { manufacturers } from "@constants";
 import { SearchManuFacturerProps } from "@types";
 
+// 2 props manufacturer và setManufacturer sẽ được pass từ SearchBar. 2 props này chính là state của SearchBar. Tức là ở đây ta sẽ gán manufacturer thành giá trị của combobox. Từ đó, thay đổi giá trị được chọn ở combobox sẽ thay đổi manufacturer từ đó update ngược lên, thay đổi state của SearchBar.
 const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacturerProps) => {
+  // tạo state để lưu input của user, giúp lọc manufacturers.
   const [query, setQuery] = useState("");
 
   const filteredManufacturers =
@@ -17,10 +20,20 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
             .replace(/\s+/g, "")
             .includes(query.toLowerCase().replace(/\s+/g, ""))
         );
+// lưu ý: replace(/\s+/g, "") is a JavaScript method that returns a new string with all the whitespace characters in the original string removed. The regular expression /\\s+/g matches one or more whitespace characters (spaces, tabs, line breaks, etc.) globally in the string, and the empty string "" is used as the replacement value. This is useful for removing spaces and other whitespace characters from a string before comparing or processing it.
+
+        // Đoạn code trên nhằm mục đích lọc ra các manufacture từ query người dùng nhập vào
+// 1. manufactures là mảng chứa các nhà sản xuất xe 
+// 2. Nếu query empty thì giả về toàn bộ manufactures
+// 3. nếu query không empty thì lọc ra và giả về các item từ manufactures thoả mãn: 
+// mang mỗi item biến toàn bộ thành in thường, lọc hết toàn bộ khoảng trắng gây ra bởi spaces, tabs, line breaks, etc..Xem nó có chứa query không (query này cũng đã được chuyển thành in thường và lọc khoảng trắng). 
+
 
   return (
     <div className='search-manufacturer'>
       <Combobox value={manufacturer} onChange={setManuFacturer}>
+        {/* gán states của SearchBar vào Combobox */}
+        {/* combobox này có tác dụng hiển thị ra list các manufacturer từ input của user. */}
         <div className='relative w-full'>
           {/* Button for the combobox. Click on the icon to see the complete dropdown */}
           <Combobox.Button className='absolute top-[14px]'>
@@ -53,6 +66,7 @@ const SearchManufacturer = ({ manufacturer, setManuFacturer }: SearchManuFacture
               className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm'
               static
             >
+              {/* nếu user có input, mà lọc không ra kết quả thì hiện create...; còn nếu có kết quả tương ứng, thì hiện list đó ra dưới dạng các option của combobox để cho user chọn. */}
               {filteredManufacturers.length === 0 && query !== "" ? (
                 <Combobox.Option
                   value={query}
--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -5,8 +5,12 @@ import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 
 import SearchManufacturer from "./SearchManufacturer";
-
+//tạo component SearchButton là child của component SearchBar
+//component này ta muốn tuỳ biến style cho nó (có giao diện khác nhau tuỳ lúc)
+// do đó,  tailwind css sẽ được pass từ SearchBar sang dưới dạng prop với tên là
+// otherClasses
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
+  // style của button được lấy từ otherClasses
   <button type='submit' className={`-ml-3 z-10 ${otherClasses}`}>
     <Image
       src={"/magnifying-glass.svg"}
@@ -17,23 +21,41 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
     />
   </button>
 );
+//const add = (a, b) => {
+//  return a + b;
+//};
+//equal with: 
+//const add = (a, b) => (a + b);
 
 const SearchBar = () => {
+  // quản lý state với useState
+  // state manufacturer của SearchBar sẽ được lấy giá trị ngược từ component con là SearchManufacturer
   const [manufacturer, setManuFacturer] = useState("");
+  // state model được thay đổi nhờ element input
   const [model, setModel] = useState("");
-
+// giúp navigation
   const router = useRouter();
-
+//handleSearch là hàm được tạo để xử lý sự kiện onSubmit của Form element
+// vì ở đây dùng typescript nên cần xác định kiểu dữ liệu cho e
+// trong typescript thì React.FormEvent<HTMLFormElement> là kiểu dữ liệu 
+// đại biểu cho Form event.
+// ta lôi e vào hàm handleSearch ở đây để có thể dùng e.prevenDefault
+// vì ở đây, ta không muốn dùng form như đúng công dụng của nó
+// nên cần preven default behavior của nó khi được submit. 
   const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     if (manufacturer.trim() === "" && model.trim() === "") {
       return alert("Please provide some input");
     }
+    //The trim() method removes whitespace from both ends of a string and returns the new string without the whitespace. If the string is empty or contains only whitespace, trim() returns an empty string
+    // check xem input có phải empty không
 
+    // nếu không empty thì biến tất thành lowercase rồi gọi hàm updateSarchparams để thay đổi giá trị URL. 
     updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
   };
 
+// khai báo hàm updateSearchParams có tác dụng thay đổi giá trị URL rồi route đến URL mới đó.
   const updateSearchParams = (model: string, manufacturer: string) => {
     // Create a new URLSearchParams object using the current URL search parameters
     const searchParams = new URLSearchParams(window.location.search);
@@ -65,6 +87,7 @@ const SearchBar = () => {
           manufacturer={manufacturer}
           setManuFacturer={setManuFacturer}
         />
+        {/* gán state manufacturer thành props của component SearchManufacturer để cập nhật giá trị từ đó lên. Do muốn dùng giá trị từ combobox của component này. */}
         <SearchButton otherClasses='sm:hidden' />
       </div>
       <div className='searchbar__item'>
